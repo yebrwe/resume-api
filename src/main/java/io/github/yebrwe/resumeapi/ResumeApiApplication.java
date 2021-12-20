dae5b26148d1275ef819944db41051fe1071b9e2
HoonKyung, Rhee <yebrwe@naver.com>
2021-12-20 15:55:38 +0900

    Initialize Spring Boot

---
+++ src/main/java/io/github/yebrwe/resumeapi/ResumeApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.yebrwe.resumeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/yebrwe/resumeapi/ResumeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.yebrwe.resumeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
